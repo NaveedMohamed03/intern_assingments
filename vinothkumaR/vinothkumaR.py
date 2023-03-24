825bbf51cbac805798e746c1abd8a93f4e17aa18
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-24 14:50:18 +0530

    pseudocode witten for each line

--- vinothkumaR/vinothkumaR.py
+++ vinothkumaR/vinothkumaR.py
@@ -1,20 +1,9 @@
-l=["vinoth","kumar","naveen","mohamadnaveed"]
+l=["vinoth","kumar","naveen","mohamadnaveed"]             #1.Take a list of strings
 for i in range(0,len(l)):
-    for j in range(i+1,len(l)):
+    for j in range(i+1,len(l)):                            #2.Two loops to iterate and check the length of the string
         if len(l[i])<len(l[j]):
-            l[i],l[j]=l[j],l[i]
+            l[i],l[j]=l[j],l[i]                            #3.After sorting print the firts string inthe sorted list
 print(l[0])
-
-
-
-
-"""
-psuedocode
-1.Take a list of strings
-2.Two loops to iterate and check the length of the string
-3.After sorting print the firts string inthe sorted list
-"""
-
 
 
 """
