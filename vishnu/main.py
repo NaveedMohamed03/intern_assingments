a1b585a832abc1054d7b25f530038daef25a2f4f
ANORA\Vishnu Teja <teja@anorasolutions.com>
2023-03-24 15:32:30 +0530

    added another three lines

--- vishnu/main.py
+++ vishnu/main.py
@@ -1,3 +1,7 @@
 def find_first_non_repeating_char(string):
     char_count = {}
     for char in string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
