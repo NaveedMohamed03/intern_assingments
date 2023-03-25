7df0f166815f4e11f0205e2b98eb3e503f1b044b
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-25 14:10:40 +0530

    merged

--- Grouped/main.py
+++ Grouped/main.py
@@ -1,34 +1,4 @@
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-class BinaryTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        new_node = Node(value)
-        if self.root is None:
-            self.root = new_node
-        else:
-            current_node = self.root
-            while True:
-                if value < current_node.value:
-                    if current_node.left is None:
-                        current_node.left = new_node
-                        return
-                    else:
-                        current_node = current_node.left
-                else:
-                    if current_node.right is None:
-                        current_node.right = new_node
-                        return
-                    else:
-                        current_node = current_node.right
-
 
 """
 QUESTION:
@@ -98,4 +68,36 @@ print(l)
 
 
 
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+        else:
+            current_node = self.root
+            while True:
+                if value < current_node.value:
+                    if current_node.left is None:
+                        current_node.left = new_node
+                        return
+                    else:
+                        current_node = current_node.left
+                else:
+                    if current_node.right is None:
+                        current_node.right = new_node
+                        return
+                    else:
+                        current_node = current_node.right
 
